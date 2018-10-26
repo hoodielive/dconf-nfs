8fb26a8441a257f53f89fb6ce0324dbf93f975d9
hoodielive <hoodielive@gmail.com>
2018-10-26 16:09:37 -0400

    dconf script.. sigh

--- tested.sh
+++ tested.sh
@@ -9,10 +9,10 @@ user=/home/$users/.config/dconf
   else
     sudo -u "$users" bash <<EOF
 	if "find /home/$users/.config/dconf -type f -iname "user" " ; then
-		echo "user-db:test" > /home/$user/.config/dconf/dconf-temporary-profile
-		env DCONF_PROFILE="dconf-temporary-profile" dconf dump / 1>/home/$users/.config/dconf/user.txt
+	  echo "user-db:test" > /home/$user/.config/dconf/dconf-temporary-profile
+	  env DCONF_PROFILE="dconf-temporary-profile" dconf dump / 1>/home/$users/.config/dconf/user.txt
 	else
-		echo "the user file is not there for $users, so operation terminated"
+	  echo "the user file is not there for $users, so operation terminated"
 	fi
   fi
 EOF
