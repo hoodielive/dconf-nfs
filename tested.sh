ba8b10c46aeae811b46bd0a4584974a961a9fff5
hoodielive <hoodielive@gmail.com>
2018-10-26 17:32:58 -0400

    run the check before <<EOF instance

--- tested.sh
+++ tested.sh
@@ -7,14 +7,14 @@ user=/home/$users/.config/dconf
   if $(find $users -type f -iname "user.txt") ; then
     echo "found in $users" 2>/dev/null
   else
-    sudo -u "$users" bash <<EOF
 	if "find /home/$users/.config/dconf -type f -iname "user" " ; then
+    sudo -u "$users" bash <<EOF
 	  echo "user-db:test" > /home/$user/.config/dconf/dconf-temporary-profile
 	  env DCONF_PROFILE="dconf-temporary-profile" dconf dump / 1>/home/$users/.config/dconf/user.txt
+EOF
 	else
 	  echo "the user file is not there for $users, so operation terminated"
 	fi
-  fi
-EOF
+ fi
 done
 
